8788ba92a209247029a727df672f4de49e67aa0b
dkphhh <dkphhh@foxmail.com>
2025-11-07 01:39:16 +0800

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	import favicon from '$lib/assets/favicon.ico';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import Notification from '$lib/components/common-components/notification/Notification.svelte';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	// import ColorModeButton from '$lib/components/ColorModeButton.svelte';
+
+	injectSpeedInsights();
 	let { children } = $props();
 	injectAnalytics();
 </script>
